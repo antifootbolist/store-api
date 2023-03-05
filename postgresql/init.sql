8c215cf4fdf5981512430c2151a206ca2c00a75d
Alexey Borodulin <antifootbolist@gmail.com>
2023-03-06 04:37:33 +1000

    Add test data

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -9,5 +9,8 @@ CREATE TABLE products (
   price INT
 );
 
+insert into products values(1, 'iPhone', 'iPhone 14', 100);
+insert into products values(2, 'iPhone', 'iPhone 14 PRO MAX', 200);
+
 CREATE USER "user-api" WITH PASSWORD 'qwe123';
 GRANT ALL PRIVILEGES ON TABLE products TO "user-api";
