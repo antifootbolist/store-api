80cb96f559e5f074cadea32c8cf73e5bcca324e4
Alexey Borodulin <antifootbolist@gmail.com>
2023-03-11 22:52:44 +1000

    Moved test data to GORM code in app

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -3,14 +3,11 @@ CREATE DATABASE store_api;
 \c store_api;
 
 CREATE TABLE products (
-  id INT,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   description VARCHAR(1024),
   price INT
 );
-
-insert into products values(1, 'iPhone', 'iPhone 14', 100);
-insert into products values(2, 'iPhone', 'iPhone 14 PRO MAX', 200);
 
 CREATE USER "user-api" WITH PASSWORD 'qwe123';
 GRANT ALL PRIVILEGES ON TABLE products TO "user-api";
