cff5da8122ac5e23aa55f22d9b3e4ac1b60d92ca
Alexey Borodulin <antifootbolist@gmail.com>
2023-03-12 06:37:53 +1000

    Fix again 2

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE store_api;
 CREATE USER "user-api" WITH PASSWORD 'qwe123';
+GRANT ALL ON SCHEMA public TO "user-api";
 GRANT ALL PRIVILEGES ON DATABASE store_api TO "user-api";
 GRANT ALL PRIVILEGES ON SCHEMA public TO "user-api";
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "user-api";
